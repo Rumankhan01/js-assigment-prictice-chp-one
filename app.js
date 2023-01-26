2ac720b83d12c7c241d52f7e8f9238ea65cd9ce8
Rumankhan01 <87278550+Rumankhan01@users.noreply.github.com>
2023-01-26 17:02:35 +0500

    Add files via upload
    javascript assigment prictice chapter one

---
+++ app.js
@@ -0,0 +1,35 @@
+// Question N0#01
+/***Write a script to Greet your website visitors using js alert box***/
+
+// alert("welcome Dear Students");
+
+
+
+// Question N0#02
+
+/****write  a script to display following massage on your web page****/
+
+// alert("Hello Ruman");
+
+// Question N0#03
+
+/****Write a script to dsiplay following massage on your web page (Hint : Use line break)*****/
+
+// alert("AlertScript Alert\n \nEroor! please enter a valid pasword");
+
+// Question No#04:
+
+/**** Write a script to display following massage in sequence****/
+
+
+// alert("Welcome to jS land");
+
+
+// Question No#05
+
+/***  Genrate the following massage through browser's console:***/
+
+// console.log("Hello i can run js through my web browser console");
+
+
+
